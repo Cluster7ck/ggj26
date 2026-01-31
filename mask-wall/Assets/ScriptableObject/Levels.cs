7a1b33c1c544a8a9900eac3724bdc017c3f9652a
Lennart Erpenbeck <8666784+Cluster7ck@users.noreply.github.com>
2026-01-31 19:29:29 +0100

    lvel

--- mask-wall/Assets/ScriptableObject/Levels.cs
+++ mask-wall/Assets/ScriptableObject/Levels.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 [Serializable]
 public class Level
 {
-    [SerializeField] private Texture2D wallTexture;
+    public Texture2D wallTexture;
 
-    [SerializeField] private GameObject player;
+    public GameObject player;
 }
--- mask-wall/Assets/Scripts/Wall.cs
+++ mask-wall/Assets/Scripts/Wall.cs
@@ -1,19 +1,39 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Wall : MonoBehaviour
 {
+  private static readonly int baseTextureName = Shader.PropertyToID("Base");
   private float initialZ;
   private List<float> zSteps = new();
 
+  private MeshRenderer meshRenderer;
+
   private void Awake()
   {
+    meshRenderer = GetComponent<MeshRenderer>();
     initialZ = transform.position.z;
+    GameController.Instance.OnLevelChange += OnLevelChange;
   }
 
-  public void Reset()
+  private void OnDestroy()
   {
+    GameController.Instance.OnLevelChange -= OnLevelChange;
+  }
+
+  public void OnLevelChange(object ev, Level level)
+  {
+    meshRenderer.material.SetTexture(baseTextureName, level.wallTexture);
+      
     var resetPost = new Vector3(transform.position.x, transform.position.y, initialZ);
     transform.position = new Vector3(transform.position.x, transform.position.y, initialZ);
+    var shape = level.player.GetComponent<Shape>();
+    zSteps.Clear();
+    //var dist = initialZ vw
+    for (int i = 0; i < shape.Joints.Count; i++)
+    {
+    }
+    
   }
 }
